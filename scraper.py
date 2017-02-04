e7ce5580e62d3b3fe8310734b3e3c07e9dc3ff26
chris48s <chris.shaw480@gmail.com>
2017-02-04 17:13:00 +0000

    update to account for new field spec

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ from dc_base_scrapers.xml_scraper import GmlScraper
 
 stations_url = "http://mapsbcw.wellingborough.gov.uk/arcgis/services/electoral/electoral_wfs/MapServer/WFSServer?service=WFS&version=1.1.0&request=GetFeature&typeNames=electoral_electoral_wfs%3APolling_Stations"
 stations_fields = {
-    '{http://bcw-giscore.wellingborough.gov.uk:6080/arcgis/services/electoral/electoral_wfs/MapServer/WFSServer}OBJECTID_12': 'OBJECTID_12',
+    '{http://bcw-giscore.wellingborough.gov.uk:6080/arcgis/services/electoral/electoral_wfs/MapServer/WFSServer}OBJECTID': 'OBJECTID',
     '{http://bcw-giscore.wellingborough.gov.uk:6080/arcgis/services/electoral/electoral_wfs/MapServer/WFSServer}ADDRESS': 'ADDRESS',
     '{http://bcw-giscore.wellingborough.gov.uk:6080/arcgis/services/electoral/electoral_wfs/MapServer/WFSServer}DISTRICTS': 'DISTRICTS',
     '{http://bcw-giscore.wellingborough.gov.uk:6080/arcgis/services/electoral/electoral_wfs/MapServer/WFSServer}WEBSITE': 'WEBSITE',
@@ -27,7 +27,7 @@ districts_fields = {
 council_id = 'E07000156'
 
 
-stations_scraper = GmlScraper(stations_url, council_id, 'stations', stations_fields, 'OBJECTID_12')
+stations_scraper = GmlScraper(stations_url, council_id, 'stations', stations_fields, 'OBJECTID')
 stations_scraper.scrape()
 districts_scraper = GmlScraper(districts_url, council_id, 'districts', districts_fields, 'OBJECTID')
 districts_scraper.scrape()
